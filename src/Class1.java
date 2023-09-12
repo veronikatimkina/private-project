b7c8b407e4918e886cf37f8c961ef5c6ec5c007e
Veronika Leko <veronika.timkina@jetbrains.com>
2023-09-12 18:06:45 +0200

    changes in Class13 2

--- src/Class1.java
+++ src/Class1.java
@@ -1,7 +1,7 @@
 public class Class1 {
 
     public static void main(String[] args) {
-        System.out.println("ererggtregrgegrgte");
+        System.out.println("ererggtregrgegefwefwefwrgte");
 
     }
 }
