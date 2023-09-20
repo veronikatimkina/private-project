2a1617dc4ac348e6c90ef76a11b84f6733ea1a83
Veronika Leko <veronika.timkina@jetbrains.com>
2023-09-20 16:19:34 +0200

    New changelistвививаививаи

--- src/Class2.java
+++ src/Class2.java
@@ -1,6 +1,6 @@
 public class Class2 {
     //masterавмиваиваиваививаи
     public void alalala(){
-//5757575757wfewfwfwefwefч ч иваиваиваиваи
+//5757575757wfewfwfwefwefч ч иваиваиваиваимс иваиваиваи
     };
 }
