1c5aa2619e0962c7b737a5100331eed022269a1d
Veronika Leko <veronika.timkina@jetbrains.com>
2023-09-20 17:15:08 +0200

    efefwefwefwfwf

--- src/Class2.java
+++ src/Class2.java
@@ -1,6 +1,6 @@
 public class Class2 {
     //masterавмиваиваиваививаицкпцкпукпукпвамипкиукпукп
     public void alalala(){
-//5757575757wfewfwfwefwefч ч иваиваиваиваимс иваиваиваиregrкцупукпукпукпукипукуиукиgerergergeefwefffwefwef
+//5757575757wfewfwfwefwefч ч иваиваиваиваимс иваиваиваиregrкцупукпукпукпукипукуиукиgerergergeefwefffwefweferfergergerg
     };
 }
